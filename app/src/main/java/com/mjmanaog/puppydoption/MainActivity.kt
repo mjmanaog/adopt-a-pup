94a97685b7d7f60a85b633a95110021db2bbd9c1
MJ Manaog <>
2021-02-27 16:37:40 +0800

    chore: removed unused files/dependencies

--- app/src/main/java/com/mjmanaog/puppydoption/MainActivity.kt
+++ app/src/main/java/com/mjmanaog/puppydoption/MainActivity.kt
@@ -14,17 +14,12 @@ import androidx.core.content.ContextCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.firebase.FirebaseApp
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import com.mjmanaog.puppydoption.helpers.ROUTE_LANDING
 import com.mjmanaog.puppydoption.helpers.ROUTE_MAIN
 import com.mjmanaog.puppydoption.network.model.PuppyModel
 import com.mjmanaog.puppydoption.network.model.puppyList
-import com.mjmanaog.puppydoption.ui.screens.*
+import com.mjmanaog.puppydoption.ui.screens.LandingScreen
+import com.mjmanaog.puppydoption.ui.screens.MainScreen
 import com.mjmanaog.puppydoption.ui.theme.PuppydoptionTheme
 
 class MainActivity : AppCompatActivity() {
@@ -32,30 +27,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        FirebaseApp.initializeApp(this)
-
         val window: Window = this.window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, R.color.holo_orange_dark)
-
-        val database = Firebase.database
-        val myRef = database.getReference("puppies")
-
-        myRef.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.getValue()
-                Log.d(">>>", "Value is: $value")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(">>>", "Failed to read value.", error.toException())
-            }
-        })
 
         setContent {
             PuppydoptionTheme {
                 PuppyApplication()
             }
+        }
+    }
+
+    fun longLog(aa : String){
+        if(aa.length > 4000){
+            Log.e("@@@", aa.substring(0,4000))
+            longLog(aa.substring(4000));
+        }else{
+            Log.e(" ", aa)
         }
     }
 }
